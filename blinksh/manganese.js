1f95447c882ad9f27cda4e44526d59382cacb08d
Diego Iaconelli <matdotcx@users.noreply.github.com>
2025-06-03 09:39:19 +0100

    Update manganese.js
    Back to basics formatting

--- blinksh/manganese.js
+++ blinksh/manganese.js
@@ -1,33 +1,4 @@
-// Manganese Deadline Dark theme for Blink Shell
-// Based on the Manganese Deadline Zed theme
-
-// Define the 16 color palette
-black       = ‘#1f1f1f’;   // background
-red         = ‘#d14d41’;   // red
-green       = ‘#879a39’;   // green
-yellow      = ‘#d0a215’;   // yellow
-blue        = ‘#4385be’;   // blue
-magenta     = ‘#ff9248’;   // orange/magenta
-cyan        = ‘#3aa99f’;   // cyan
-white       = ‘#e8e8e8’;   // light gray
-
-lightBlack  = ‘#525561’;   // medium gray
-lightRed    = ‘#d14d41’;   // bright red
-lightGreen  = ‘#879a39’;   // bright green
-lightYellow = ‘#d0a215’;   // bright yellow
-lightBlue   = ‘#4385be’;   // bright blue
-lightMagenta= ‘#ff9248’;   // bright orange/magenta
-lightCyan   = ‘#3aa99f’;   // bright cyan
-lightWhite  = ‘#f0f0f0’;   // white
-
-// Set the color palette
-t.prefs_.set(‘color-palette-overrides’,
-[ black , red     , green  , yellow,
-blue     , magenta , cyan   , white,
-lightBlack   , lightRed  , lightGreen , lightYellow,
-lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
-
-// Set cursor, foreground and background colors
-t.prefs_.set(‘cursor-color’, ‘#ff9248’);
-t.prefs_.set(‘foreground-color’, ‘#e8e8e8’);
-t.prefs_.set(‘background-color’, ‘#1f1f1f’);
+t.prefs_.set(‘color-palette-overrides’,[”#1f1f1f”, “#d14d41”, “#879a39”, “#d0a215”, “#4385be”, “#ff9248”, “#3aa99f”, “#e8e8e8”, “#525561”, “#d14d41”, “#879a39”, “#d0a215”, “#4385be”, “#ff9248”, “#3aa99f”, “#f0f0f0”]);
+t.prefs_.set(‘foreground-color’, “#e8e8e8”);
+t.prefs_.set(‘background-color’, “#1f1f1f”);
+t.prefs_.set(‘cursor-color’, ‘rgba(255,146,72,0.8)’);
